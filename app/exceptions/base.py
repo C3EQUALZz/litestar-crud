764122a56065d2c6ce17d7ca7bf5adf6f4d922b4
matebookWindows <dan.kovalev2013@gmail.com>
2025-04-28 16:44:21 +0300

    added base class for all base exceptions

---
+++ app/exceptions/base.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass(eq=False)
+class BaseAppError(Exception, ABC):
+    """
+    Base error class from each layer must be inherited.
+    For example, I have several layers according to DDD:
+    - application
+    - domain
+    - logic
+    - infrastructure
+    """
+    message: str
+
+    @property
+    @abstractmethod
+    def status(self) -> int:
+        """
+        HTTP status code of exception.
+        :return: number of HTTP status code. For example, 404, 500 and etc.
+        """
+        raise NotImplementedError
+
+    @property
+    def headers(self) -> dict[str, str] | None:
+        """
+        Headers for exception.
+        For example, you can write here information about bearer token and etc.
+        :return: dictionary of headers
+        """
+        raise NotImplementedError
+
+    def __str__(self) -> str:
+        """
+        :return: The error message left by the developer
+        """
+        return self.message
