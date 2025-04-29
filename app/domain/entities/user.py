411e0372efefc51bfa9fd0b5df51c5c165f305a6
matebookWindows <dan.kovalev2013@gmail.com>
2025-04-29 14:04:33 +0300

    added value object for password

--- app/domain/entities/user.py
+++ app/domain/entities/user.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 
 from app.domain.entities.base import BaseEntity
-from app.domain.values.user import UserName, UserSurname
+from app.domain.values.user import Password, UserName, UserSurname
 
 
 @dataclass(eq=False)
 class UserEntity(BaseEntity):
     name: UserName
     surname: UserSurname
-    password: bytes
+    password: Password
--- app/domain/values/user.py
+++ app/domain/values/user.py
@@ -1,9 +1,11 @@
+from dataclasses import dataclass
 from typing import override
 
 from app.domain.values.base import BaseValueObject
-from app.exceptions.domain import CantBeNumberError, EmptyFieldError, WrongTypeError
+from app.exceptions.domain import CantBeNumberError, EmptyFieldError, EmptyPasswordError, WrongTypeError
 
 
+@dataclass
 class UserName(BaseValueObject[str]):
     value: str
 
@@ -23,4 +25,19 @@ class UserName(BaseValueObject[str]):
         return str(self.value)
 
 
+@dataclass
 class UserSurname(UserName): ...
+
+
+@dataclass
+class Password(BaseValueObject[bytes]):
+    value: bytes
+
+    @override
+    def validate(self) -> None:
+        if not self.value:
+            raise EmptyPasswordError(f"Value {self.value} is empty, please provide info")
+
+    @override
+    def as_generic_type(self) -> bytes:
+        return self.value
